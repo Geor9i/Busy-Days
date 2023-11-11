7993a328887580fea0fccc8ca2034045485fa464
Georgi <gurumov93@gmail.com>
2023-11-11 19:53:40 +0000

    Fireabase Inital project setup and hosting
    Initialized Firebase in project directory and hosted on link: https://busy-dayz.web.app/

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,20 @@ import Members from "./components/Members/Members.jsx";
 import LoginPage from "./components/Login/loginPage.jsx";
 import SignUpPage from "./components/SignUp/SignUpPage.jsx";
 import NotFound from "./components/Errors/NotFound.jsx";
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8wCR98lowGhbXrN_bPuUw4D-WaenIbDQ",
+  authDomain: "busy-dayz.firebaseapp.com",
+  projectId: "busy-dayz",
+  storageBucket: "busy-dayz.appspot.com",
+  messagingSenderId: "668014678034",
+  appId: "1:668014678034:web:040b52eec494c72c15c8cb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
