1c54022d7f6e06afb90c5a39aa6ed4f8e9433a9b
Georgi <gurumov93@gmail.com>
2023-11-18 21:42:12 +0000

    Created Dropdown content-container per employee in employee view to work as a details section

--- src/components/EmployeeView/EmployeeListItem.jsx
+++ src/components/EmployeeView/EmployeeListItem.jsx
@@ -1,14 +1,28 @@
-import styles from './employeeListItem.module.css'
+import { useState } from "react";
+import styles from "./employeeListItem.module.css";
 
 export default function EmployeeListItem() {
-    return (
-        <tr className={styles['employee-list-item']}>
-        <td className={styles['firstName-td']}>Bimala</td>
-        <td className={styles['lastName-td']}>Sharma</td>
-        <td className={styles['updatedOn-td']}>Full time</td>
-        <td className={styles['positions-td']}>MOH, FOH</td>
-        <td className={styles['createdOn-td']}>17/11/2023</td>
-        <td className={styles['updatedOn-td']}>17/11/2023</td>
+  const [show, setShow] = useState(false);
+  const showEmployeeDetails = (e) => {
+    setShow(state => !state);
+}
+  return (
+    <>
+      <tr onClick={showEmployeeDetails} className={styles["employee-list-item"]}>
+        <td className={styles["firstName-td"]}>Bimala</td>
+        <td className={styles["lastName-td"]}>Sharma</td>
+        <td className={styles["updatedOn-td"]}>Full time</td>
+        <td className={styles["positions-td"]}>MOH, FOH</td>
+        <td className={styles["createdOn-td"]}>17/11/2023</td>
+        <td className={styles["updatedOn-td"]}>17/11/2023</td>
       </tr>
-    )
+      <tr>
+        <td colSpan={6} className={styles["employee-details"]}>
+          <div className={`${styles['employee-details-content-container']} ${show ? '' : styles['collapse']}`}>
+          <div className={styles['employee-details-content']}></div>
+          </div>
+        </td>
+      </tr>
+    </>
+  );
 }
